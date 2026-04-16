e68671a7e31b8e5d6c7d7aed84d4b0a5c636ffc8
Tshepo Matshisevhe <Tshepo.Matshisevhe@investec.com>
2026-04-16 10:13:39 +0200

    Improved the CoffeeMaker using OOP

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -2,20 +2,32 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-money_machine = MoneyMachine()
-coffee_maker = CoffeeMaker()
-the_menu = Menu()
-is_on = True
+def main():
+    money_machine = MoneyMachine()
+    coffee_maker = CoffeeMaker()
+    menu = Menu()
+    machine_on = True
 
+    while machine_on:
 
-while is_on:
-    options = the_menu.get_items()
-    choice = input(f"What would you like? ({options})   ")
-    if choice == "off":
-        is_on = False
-    elif choice == "report":
-        coffee_maker.report()
-        money_machine.report()
-    else:
-        drink = the_menu.find_drink(choice)
-        print(coffee_maker.is_resource_sufficient(drink))
+        choice = input(f"What would you like? ({menu.get_items()}):   ").strip().lower()
+
+        if choice == "off":
+            machine_on = False
+        elif choice == "report":
+            coffee_maker.report()
+            money_machine.report()
+        else:
+            drink = menu.find_drink(choice)
+
+            if drink is None:
+                continue
+
+            if not coffee_maker.is_resource_sufficient(drink):
+                continue
+
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+if __name__ == "__main__":
+    main()
