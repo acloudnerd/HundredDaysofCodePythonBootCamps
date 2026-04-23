8aac78b403cf1d9971fd8ceded9ea63a1b452182
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-04-23 06:07:21 +0200

    random colors generated by turtle

---
+++ Day 18/random_color.py
@@ -0,0 +1,21 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+# Use the Python Turtle documentation to find more colors or set colormode
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+directions = [0, 90, 180, 270]
+tim.pensize(10)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
