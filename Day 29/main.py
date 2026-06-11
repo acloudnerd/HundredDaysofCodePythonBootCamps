2ba52a1295c5009b0eb2a898ced4dc9634f773fe
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-06-11 14:43:13 +0200

    Project setup for the password manager

---
+++ Day 29/main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+CANVAS_W, CANVAS_H = 200, 250
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+window.resizable(False, False)
+
+Canvas(width=CANVAS_W, height=CANVAS_H)
+
+window.mainloop()
