29e4be324c63438e4e41e10c70a4d80d282de922
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-06-16 01:31:24 +0200

    password manager project setup

--- Day 29/main.py
+++ Day 29/main.py
@@ -1,12 +1,26 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
-CANVAS_W, CANVAS_H = 200, 250
+# constants
+CANVAS_W, CANVAS_H = 200, 200
+PINK = "#e2979c"
 
+
+# creating the window
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 window.resizable(False, False)
 
-Canvas(width=CANVAS_W, height=CANVAS_H)
+# setting up and showing the canvas
+canvas = Canvas(width=CANVAS_W, height=CANVAS_H)
+
+pil_image = Image.open("logo.png").resize((CANVAS_W, CANVAS_H)).convert("RGBA")
+background = Image.new("RGBA", (CANVAS_W, CANVAS_H), PINK)
+background.paste(pil_image, mask=pil_image.split()[3])
+logo_img = ImageTk.PhotoImage(background.convert("RGB"))
+canvas.create_image(CANVAS_W // 2, CANVAS_H // 2, image=logo_img)
+canvas.grid(column=1, row=1)
+
 
 window.mainloop()
